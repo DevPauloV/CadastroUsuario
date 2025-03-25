3ba6504112bee600af5b50e5966276e5e1fd4603
Paulo Victor <vitorcanalle@gmail.com>
2025-03-24 22:36:35 -0300

    criação do formulario

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,21 +1,36 @@
 
 import './style.css'
+import Trash from 'C:\Users\vitor\dev-cadastrousuario\src\trash.svg'
+
 
 function Home() {
-  
+
 
   return (
-      <div className='container'>
+    <div className='container'>
       <form>
         <h1>Cadastro de Usuários</h1>
-        <input name='nome' type='text'/>
-        <input name='idade' type='number'/>
-        <input name='email' type='email'/>
+        <input name='nome' type='text' />
+        <input name='idade' type='number' />
+        <input name='email' type='email' />
         <button type='button'>Cadastrar</button>
       </form>
-      
+
+      <div>
+        <div>
+          <p>Nome:</p>
+          <p>Idade:</p>
+          <p>Email:</p>
+        </div>
       </div>
-      
+      <button>
+        <img src={Trash} />
+      </button>
+
+
+
+    </div>
+
   )
 }
 
